be04c0e7bcca8f35e0cf40c409c321039bc32ace
Stan <sk2114@outlook.com>
2022-09-19 15:35:09 +0300

    Minor Changes

--- CreatingTasks/Program.cs
+++ CreatingTasks/Program.cs
@@ -4,10 +4,10 @@ var task = Task.Run(() =>
             Console.WriteLine("Heavy Task Running");
             Task.Delay(1000);
             Console.WriteLine("Heavy Task Complete");
-            return new object();
+            return new string("Heavy Task Result");
         }
     );
 Console.WriteLine("Execution resuming in main thread");
 var resultingObj = await task;
-Console.WriteLine(resultingObj.ToString());
+Console.WriteLine(resultingObj);
 
